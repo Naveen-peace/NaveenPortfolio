2c1649e36ac75576d73d8f211de453490dc28fc2
Naveen-peace <naveenshark49@gmail.com>
2025-06-06 10:05:04 +0530

    another commit projects

---
+++ src/component/data/FreelanceData.jsx
@@ -0,0 +1,33 @@
+import Project9 from '../../assets/image/react-webpage4.png';
+import Project10 from '../../assets/image/react-webpage5.png';
+import Project12 from '../../assets/image/expo-project.jpeg';
+import Project13 from '../../assets/image/react-webpage-7.png';
+import Project16 from '../../assets/image/react-webpage-10.png';
+import Project17 from '../../assets/image/react-webpage-11.png';
+import Project18 from '../../assets/image/react-webpage-12.png';
+
+const userFrelanceNaveen = {
+    name: 'Naveen Kumar.S',
+    phoneNumber: '9841601822',
+    email: 'naveenshark49@gmail.com',
+    education: 'B.tech IT',
+    course: 'MERN FullStack Developer',
+    certificate: '',
+    work: 'Fresher',
+    socialMedia: [
+        { instagramLink: 'https://www.instagram.com/dark_devil_joke/' },
+        { linkedinLink: 'https://www.linkedin.com/in/naveen-kumar-b6b1a6247/' },
+        { githubLink: 'https://github.com/Naveen-peace' },
+        { EmailId: 'naveenshark49@gmail.com' },
+    ],
+    allprojects: [
+        { project: Project9, name: 'sagiServices', language: 'HTML | CSS | JS | React.js ', title: 'A static webpage specializing in business consulting services', githubLink: 'https://github.com/Naveen-peace', Livelink: 'https://sagiservices.com/' },
+        { project: Project10, name: 'HotelRoom-Booking', language: 'HTML | CSS | JS | React.js ', title: 'A static webpage design for Hotel Rooms Booking services', githubLink: 'https://github.com/Naveen-peace/HotelRooms', Livelink: 'https://hotelroom-booking.netlify.app/' },
+        { project: Project12, name: 'Horoscope-app', language: 'HTML | CSS | JS | React-Expo ', title: 'It is Astrology form to download our Astrology details  in Pdf format using this Horoscope app ', githubLink: 'https://github.com/Naveen-peace', Livelink: '' },
+        { project: Project13, name: 'gamesstore', language: 'HTML | CSS | JS | React.js ', title: 'God of War is a series of action video games store webpage ', githubLink: 'https://github.com/Naveen-peace/gamesstore', Livelink: 'https://allgamestore.netlify.app/' },
+        { project: Project16, name: 'Kanchi Runners', language: 'HTML | CSS | JS | React.js ', title: 'This is a Single-page website created to showcase my experience and projects. It is not a demo site—its a freelancing Website', githubLink: 'https://github.com/Naveen-peace/', Livelink: 'https://www.kanchirunners.com/' },    
+        { project: Project17, name: 'orthoinvister', language: 'HTML | CSS | JS | React.js ', title: 'This is a Landing-page website created to showcase my experience and projects. It is not a demo site—its a freelancing Website', githubLink: 'https://github.com/Naveen-peace/', Livelink: 'https://orthoinvister.netlify.app/' },
+        { project: Project18, name: 'Printing and design', language: 'HTML | CSS | JS | React.js ', title: 'This is a Landing-page website created to showcase my experience and projects. It is not a demo site—its a freelancing Website', githubLink: 'https://github.com/Naveen-peace/', Livelink: 'https://saidesinerandprinter.netlify.app/' },
+    ],
+};
+export default userFrelanceNaveen;
--- src/component/pages/subpages/Project.jsx
+++ src/component/pages/subpages/Project.jsx
@@ -2,6 +2,7 @@ import { Icon } from "@iconify/react"
 import Footer from "../footer/Footer"
 import Header from "../header/Header"
 import userNaveen from "../../data/Data"
+import userFrelanceNaveen from '../../data/FreelanceData'
 
 
 function Project() {
@@ -10,9 +11,47 @@ function Project() {
       <Header />
       <div className="lol-container">
         <div className="project-card">
+           <div className="project-card-sub">
+             <h2>My Freelance Projects</h2>
+            <h3>Recent client work</h3>
+          </div>
+           <div className="corrcetion-card">
+            <div className="project-card-1">
+              {userFrelanceNaveen.allprojects.map((project, index) => (
+                <div className="project-1" key={index}>
+                  <img
+                    style={{ width: '100%', height: '185px', }}
+                    src={project.project}
+                    alt={`project`}
+                  />
+                  <div className="project-card-2">
+                    <div className="project-2">
+                      <h2>{project.name}</h2>
+                      <h4>{project.title}</h4>
+                      <h4>{project.language}</h4>
+                    </div>
+                    <div className="project-livebutton">
+                      <button className="livebutton" >
+                        <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
+                          <Icon icon="devicon:github" width="40" height="30" />
+                          GitHub
+                        </a>
+                      </button>
+                      <button className="livebutton" >
+                        <a href={project.Livelink} target="_blank" rel="noopener noreferrer">
+                          <Icon icon="basil:share-box-solid" width="40" height="30" />
+                          Live
+                        </a>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
           <div className="project-card-sub">
-            <h2>Projects</h2>
-            <h3>My recent works</h3>
+             <h2>Demo & Learning Projects</h2>
+            <h3>Personal practice & learning outcomes</h3>
           </div>
           <div className="corrcetion-card">
             <div className="project-card-1">
